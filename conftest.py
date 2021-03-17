5700edd23fb36d738f83c93271fae9bc53043e68
ctroszek <79866324+ctroszek@users.noreply.github.com>
2021-03-17 20:57:38 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,24 +4,24 @@ import psycopg2
 import select
 
 
-@pytest.fixture()
-def browser():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
-
-
 # @pytest.fixture()
 # def browser():
-#     chrome_options = webdriver.ChromeOptions()
-#     chrome_options.add_argument('--no-sandbox')
-#     chrome_options.add_argument('--headless')
-#     chrome_options.add_argument('--disable-gpu')
-#     driver = webdriver.Chrome(chrome_options=chrome_options)
-#     driver.maximize_window()
-#     driver.implicitly_wait(5)
+#     driver = webdriver.Chrome()
 #     yield driver
 #     driver.quit()
+
+
+@pytest.fixture()
+def browser():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
 
 
 def group_add():
