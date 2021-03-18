7d8f52c44c766ccef8b2bf9f082a410aebadd0aa
ctroszek <79866324+ctroszek@users.noreply.github.com>
2021-03-19 02:35:55 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -29,12 +29,12 @@ def setup_teardown():
 
 @pytest.fixture()
 def browser():
-    chrome_options = webdriver.ChromeOptions()
+    chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     browser = webdriver.Chrome(options=chrome_options)
-    browser.maximize_window()
+    # browser.maximize_window()
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
