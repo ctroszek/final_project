8c0e92a29838921a04cea5a7e953b4d224f127b5
ctroszek <79866324+ctroszek@users.noreply.github.com>
2021-03-19 02:16:06 +0300

    Delete locators directory

--- locators/login_page_locator.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginPageLocator:
-    LOCATOR_LOGIN_PAGE_TITLE = (By.XPATH, '//a[@href="/admin/"]')
-    LOCATOR_USERNAME_FIELD = (By.XPATH, '//input[@id="id_username"]')
-    LOCATOR_PASSWD_FIELD = (By.XPATH, '//*[@id="id_password"]')
-    LOCATOR_BUTTON_LOG_IN = (By.XPATH, '//input[@type="submit"]')
--- locators/main_page_locator.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class MainPageLocators:
-    LOCATOR_GO_TO_ADMIN_BUTTON = (By.CSS_SELECTOR, '.btn.btn-primary.my-2')
