268e155c6f6535db648521d878cce55b0c6ea955
ctroszek <79866324+ctroszek@users.noreply.github.com>
2021-03-19 14:38:00 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -32,8 +32,8 @@ def browser():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    browser = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    browser = webdriver.Chrome('/usr/local/bin/chromedriver' ,chrome_options=chrome_options)
     # browser.maximize_window()
     browser.implicitly_wait(5)
     yield browser
